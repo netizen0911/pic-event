78dacedc828fa8ce89cb6277787d921f0837d23f
netizen0911 <paulo.guetan@gmail.com>
2015-08-03 17:07:17 +0800

    Initial files added.

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PIC Event with Brian Tracy</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div id="wrapper" class="container">
+			<header id="header" class="container">
+				<div id="header-inner" class="row">
+					<div class="col-md-6 col-left">
+						<div id="logo">
+							<img src="img/logo.png" />
+						</div>
+						<div id="title">
+							<p class="pic">PRIVATE INVESTMENT CLUB</p>
+							<p class="pre">Presents</p>
+							<h2 class="sub-title">A Special 2-Day Event With</h2>
+							<h1 class="main-title">BRIAN TRACY</h1>
+							<h3 class="event-info">Mississauga, August 10th, 2015</h3>
+						</div>
+					</div>
+					<div class="col-md-6 col-right">
+					</div>
+				</div><!-- #header-inner -->
+			</header><!-- #header -->
+			<div id="content" class="container">
+				<div id="content-inner" class="row">
+				</div><!-- #content-inner -->
+			</div><!-- #content -->
+			<footer id="footer" class="container">
+				<div id="footer-inner" class="row">
+				</div><!-- #footer-inner -->
+			</footer><!-- #footer -->
+		</div><!-- #wrapper -->
+		<script src="js/jquery-2.1.3.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
