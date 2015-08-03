422817a374cc5816f3a29e834c7ae1c80083e746
netizen0911 <paulo.guetan@gmail.com>
2015-08-03 23:28:57 +0800

    Finished index.php layout. Added some images.

--- index.php
+++ index.php
@@ -4,9 +4,31 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>PIC Event with Brian Tracy</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="css/flipTimer.css" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<style>
+		    .flipTimer {
+		      margin: 100px auto 0;
+		      max-width:800px;
+		      width:100%;
+		      text-align:center
+		    }
+		    .flip-cont {
+		      height: 100%;
+		      display: inline-block;
+		    }
+		    .flip-cont label {
+		        font-size: 20px;
+		    }
+		    .label label{
+		        vertical-align: top;
+		        position:relative;
+		        top:-45px
+		    }
+		</style>
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -38,16 +60,216 @@
 					</div>
 				</div><!-- #header-inner -->
 			</header><!-- #header -->
+			<div id="countdown" class="container">
+				<div class="row">
+					<div class="flipTimer">
+					    <div class="flip-cont">
+					        <div class="days"></div>
+					        <div class="label">
+					          <label>Days</label>
+					        </div>
+					    </div>    
+					    <div class="flip-cont">
+					        <div class="hours"></div>
+					        <div class="label">
+					          <label>Hours</label>
+					        </div>
+					    </div>
+					    <div class="flip-cont">
+					        <div class="minutes"></div>
+					        <div class="label">
+					          <label>Minutes</label>
+					        </div>
+					    </div>
+					    <div class="flip-cont">
+					        <div class="seconds"></div>
+					        <div class="label">
+					          <label>Seconds</label>
+					        </div>
+					    </div>
+					</div>
+				</div>
+			</div>
 			<div id="content" class="container">
-				<div id="content-inner" class="row">
-				</div><!-- #content-inner -->
+				<section id="intro" class="section-one row">
+					<h1 class="section-title">Cras porttitor varius lacus ac mollis</h1>
+					<div class="separator-line"></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, sem eget dapibus faucibus, massa magna tincidunt justo, in sodales felis justo cursus nisi. Cras ullamcorper dolor sit amet turpis ultricies, vitae euismod velit pulvinar. Sed mattis in mauris quis placerat. Cras porttitor varius lacus ac mollis. Sed euismod nisi dapibus, tempor ligula quis, efficitur libero. Curabitur sed urna ex. Aenean eu mattis arcu, vitae tempus augue. Quisque fermentum porta tincidunt. Curabitur ac turpis consectetur, bibendum erat a, hendrerit eros.</p>
+				</section>
+				<section id="carousel" class="section-two row">
+					<img src="img/tile-01.jpg" />
+					<img src="img/tile-02.jpg" />
+					<img src="img/tile-03.jpg" />
+					<img src="img/tile-04.jpg" />
+				</section>
+				<section id="authors" class="section-three row">
+					<div class="col-md-3">
+						<img src="img/icon-sunil.png" />
+						<h5>Sunil Tulsiani</h5>
+						<p>Founder of <strong>Private Investment Club</strong></p>
+					</div>
+					<div class="col-md-9">
+						<h3>Nullam efficitur quis augue id ultrices. In imperdiet cursus purus vitae aliquam.</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec venenatis nibh, in maximus tellus. Cras id pharetra felis. Praesent blandit metus nisl, et dictum libero fringilla at. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales. Nulla eget velit nisl. Nullam efficitur quis augue id ultrices. In imperdiet cursus purus vitae aliquam.
+						</p>
+					</div>
+					<div class="separator-line"></div>
+					<div class="col-md-3">
+						<img src="img/icon-brian.png" />
+						<h5>Brian Tracy</h5>
+						<p>Author and Business Coach</strong></p>
+					</div>
+					<div class="col-md-9">
+						<h3>Nullam efficitur quis augue id ultrices. In imperdiet cursus purus vitae aliquam.</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec venenatis nibh, in maximus tellus. Cras id pharetra felis. Praesent blandit metus nisl, et dictum libero fringilla at. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales. Nulla eget velit nisl. Nullam efficitur quis augue id ultrices. In imperdiet cursus purus vitae aliquam.
+						</p>
+					</div>
+				</section>
+				<section id="parallax" class="section-four row">
+					<h1 class="section-title">Cras quis tristique ante Nullam condimentum</h1>
+					<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales.</p>
+					<p><a href="#" class="cta">RESERVE YOUR SEAT NOW</a></p>
+				</section>
+				<section id="teaser" class="section-five row">
+					<div class="col-md-6">
+						<img src="img/brian-2.jpg" />
+					</div>
+					<div class="col-md-6">
+						<h3>Duis porta quam ut quam congue sodales.</h3>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue.</p>
+						<p><a href="" class="cta2">RESERVED YOUR SEAT NOW</a></p>
+					</div>
+					<div class="separator-line"></div>
+					<div class="col-md-6">
+						<img src="img/sunil-2.jpg" />
+					</div>
+					<div class="col-md-6">
+						<h3>Duis porta quam ut quam congue sodales.</h3>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue.</p>
+						<p><a href="" class="cta2">RESERVED YOUR SEAT NOW</a></p>
+					</div>
+				</section>
+				<section id="event-details" class="section-six row">
+					<h1 class="section-title-light">Cum sociis natoque penatibus et magnis dis parturient montesv</h1>
+					<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales.</p>
+					<ul>
+						<li>Cum sociis natoque penatibus et magnis dis parturient montes.</li>
+						<li>Nascetur ridiculus mus. Cras quis tristique ante.</li>
+						<li>Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus.</li>
+						<li>In tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales.
+						<li>Duis porta quam ut quam congue sodales.</li>
+					</ul>
+					<p>
+						<a href="" class="cta3">RESERVE YOUR SEAT NOW</a>
+					</p>
+				</section>
+				<section id="speakers" class="section-seven row">
+					<h1 class="section-title-lg">parturient montesv</h1>
+					<div class="col-md-3">
+						<p class="photo"><img src="img/spkr-1.jpg" /></p>
+						<h5>NICKY BILLOU</h5>
+						<p class="title">Thinking Coach, Finish Line Thinking</p>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales. Nullam condimentum, orci varius lacinia imperdiet, </p>
+					</div>
+					<div class="col-md-3">
+						<p class="photo"><img src="img/spkr-1.jpg" /></p>
+						<h5>JO AMICK</h5>
+						<p class="title">Executive Director, Investor Alliance<br/>Asset Management Group</p>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales. Nullam condimentum, orci varius lacinia imperdiet, </p>
+					</div>
+					<div class="col-md-3">
+						<p class="photo"><img src="img/spkr-1.jpg" /></p>
+						<h5>ROBERT ALLEN</h5>
+						<p class="title">Managing Director, Bramptonian Group</p>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales. Nullam condimentum, orci varius lacinia imperdiet, </p>
+					</div>
+				</section>
+				<section id="signup" class="section-eight row">
+					<h1 class="section-title-lg">REGISTER NOW</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec venenatis nibh, in maximus tellus. Cras id pharetra felis. Praesent blandit metus nisl, et dictum libero fringilla at.</p>
+					<div class="col-md-6">
+						<div id="general" class="plan">
+							<h2>GENERAL<span>ADMISSION</span></h2>
+							<div class="price">
+								<span class="sign">$</span>
+								<span class="amount">47</span>
+								<span class="cents">.00</span>
+							</div>
+							<ul>
+								<li>Duis porta quam ut quam congue</li>
+								<li>Sodales ulla eget velit nisl</li>
+								<li>Nullam efficitur quis augue</li>
+								<li>Id ultrices In imperdiet cursus</li> 
+								<li>Purus vitae aliquam augue</li>
+								<li>Nullam efficitur quis augue</li>
+								<li>Id ultrices In imperdiet cursus</li> 
+								<li>Purus vitae aliquam</li>
+							</ul>
+							<p class="btn">
+								<a href="#">REGISTER NOW</a>
+							</p>
+						</div>
+						<p class="separator"></p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec venenatis nibh, in maximus tellus. Cras id pharetra felis. Praesent blandit metus nisl, et dictum libero fringilla at. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras quis tristique ante. Nullam condimentum, orci varius lacinia imperdiet, mi tellus volutpat lectus, in tincidunt eros felis ac augue. Duis porta quam ut quam congue sodales. Nulla eget velit nisl. Nullam efficitur quis augue id ultrices. In imperdiet cursus purus vitae aliquam.</p>
+					</div>
+					<div class="col-md-6">
+						<div id="vip" class="plan">
+							<h2>VIP<span>TICKET</span></h2>
+							<div class="price">
+								<span class="sign">$</span>
+								<span class="amount">97</span>
+								<span class="cents">.00</span>
+							</div>
+							<ul>
+								<li>Duis porta quam ut quam congue</li>
+								<li>Sodales ulla eget velit nisl</li>
+								<li>Nullam efficitur quis augue</li>
+								<li>Id ultrices In imperdiet cursus</li> 
+								<li>Purus vitae aliquam augue</li>
+								<li>Nullam efficitur quis augue</li>
+								<li>Id ultrices In imperdiet cursus</li> 
+								<li>Purus vitae aliquam</li>
+							</ul>
+							<p class="btn">
+								<a href="#">REGISTER NOW</a>
+							</p>
+						</div>
+					</div>
+				</section>
 			</div><!-- #content -->
 			<footer id="footer" class="container">
-				<div id="footer-inner" class="row">
-				</div><!-- #footer-inner -->
+				<div id="social" class="row">
+					<h4>Share To Friends</h4>
+					<ul>
+						<li><a href=""><i class="facebook"></i></a></li>
+						<li><a href=""><i class="twitter"></i></a></li>
+						<li><a href=""><i class="google-plus"></i></a></li>
+						<li><a href=""><i class="linkedin"></i></a></li>
+					</ul>
+				</div>
+				<div id="copy">
+					<p>&copy; 2015 Copyright. Private Investment Club, Inc. All Rights Reserved</p>
+				</div>
 			</footer><!-- #footer -->
 		</div><!-- #wrapper -->
 		<script src="js/jquery-2.1.3.min.js"></script>
 		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="countdown/js/jquery.flipTimer.js"></script>
+		<script>
+			$(document).ready(function() {
+			  //Callback works only with direction = "down"
+			  $('.flipTimer').flipTimer({ 
+			    direction: 'down', 
+			    date: 'October 5, 2015 08:30:30',
+			    seconds: false,
+			    minutes: false,
+			    hours: false,
+			    days: false, 
+			    //callback: function() { alert('times up!'); } 
+			  });
+			});
+		</script>
 	</body>
 </html>
